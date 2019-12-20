8def123caeacedb42d896c27a5fb1e5df763aa8a
Marcel Benders <marcel.benders@cleverbridge.com>
2019-12-20 14:46:56 +0100

    (master) Initial commit

---
+++ Slipways.API/Controllers/SlipwaysController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Slipways.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SlipwaysController : ControllerBase
+    {
+
+        private readonly ILogger<SlipwaysController> _logger;
+
+        public SlipwaysController(ILogger<SlipwaysController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            return null;
+        }
+    }
+}

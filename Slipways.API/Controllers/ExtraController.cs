9da93f251b465792f8da6a1078ba2dc1526314ed
Marcel Benders <marcel.benders@cleverbridge.com>
2019-12-20 16:12:32 +0100

    (dev) Update Extra and Water Controller

--- Slipways.API/Controllers/ExtraController.cs
+++ Slipways.API/Controllers/ExtraController.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace com.b_velop.Slipways.API.Controllers
 {
-    public class ExtraController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExtraController : ControllerBase
     {
+        public ExtraController()
+        {
+        }
     }
 }

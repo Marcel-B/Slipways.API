5cf2f4b1e1fd86ae66dbbe98cfa2de16bcf51224
Marcel Benders <marcel.benders@icloud.com>
2019-12-27 01:10:29 +0100

    (dev) Add TestController

---
+++ Slipways.API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace com.b_velop.Slipways.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}

1a5b9cbe9f3871e60adda7635e5d62d2eb7d154a
Marcel Benders <marcel.benders@icloud.com>
2019-12-29 01:56:55 +0100

    (dev) Update data version

--- Slipways.API/Startup.cs
+++ Slipways.API/Startup.cs
@@ -1,14 +1,8 @@
 using System;
 using com.b_velop.Slipways.API.Infrastructure;
-using com.b_velop.Slipways.Data;
-using com.b_velop.Slipways.Data.Contracts;
 using com.b_velop.Slipways.Data.Extensions;
-using com.b_velop.Slipways.Data.Repositories;
-using IdentityServer4.AccessTokenValidation;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,59 +27,32 @@ namespace Slipways.API
             IServiceCollection services)
         {
             services.AddControllers();
-            var authority = Environment.GetEnvironmentVariable("AUTHORITY");
-            var apiResource = Environment.GetEnvironmentVariable("API_RESOURCE");
-            var cache = Environment.GetEnvironmentVariable("CACHE");
-            services.AddSlipwaysData(cache);
 
-            services.AddDbContext<SlipwaysContext>(options =>
+            var secretProvider = new SecretProvider();
+            var port = Environment.GetEnvironmentVariable("PORT");
+            var server = Environment.GetEnvironmentVariable("SERVER");
+            var user = Environment.GetEnvironmentVariable("USER");
+            var database = Environment.GetEnvironmentVariable("DATABASE");
+
+            var pw = string.Empty;
+
+            if (WebHostEnvironment.IsStaging())
             {
-                var secretProvider = new SecretProvider();
-
-                var port = Environment.GetEnvironmentVariable("PORT");
-                var server = Environment.GetEnvironmentVariable("SERVER");
-                var user = Environment.GetEnvironmentVariable("USER");
-                var database = Environment.GetEnvironmentVariable("DATABASE");
-
-                var pw = string.Empty;
-
-                if (WebHostEnvironment.IsStaging())
-                {
-                    pw = secretProvider.GetSecret("dev_slipway_db");
-                }
-                else if (WebHostEnvironment.IsProduction())
-                {
-                    pw = secretProvider.GetSecret("sqlserver");
-                }
-                else
-                {
-                    pw = "foo123bar!";
-                }
-
-                var str = $"Server={server},{port};Database={database};User Id={user};Password={pw}";
+                pw = secretProvider.GetSecret("dev_slipway_db");
+            }
+            else if (WebHostEnvironment.IsProduction())
+            {
+                pw = secretProvider.GetSecret("sqlserver");
+            }
+            else
+            {
+                pw = "foo123bar!";
+            }
+            var str = $"Server={server},{port};Database={database};User Id={user};Password={pw}";
 #if DEBUG
-                str = $"Server=db,1433;Database=Slipways;User Id=sa;Password=foo123bar!";
+            str = $"Server=db,1433;Database=Slipways;User Id=sa;Password=foo123bar!";
 #endif
-                options.UseSqlServer(str);
-            });
-
-            //services.AddAuthorization(options =>
-            //{
-            //    options.AddPolicy("reader", builder =>
-            //    {
-            //        builder.RequireScope("slipways.api.reader");
-            //    });
-            //    options.AddPolicy("allin", builder => builder.RequireScope("slipways.api.allaccess"));
-            //});
-
-            //services
-            //    .AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-            //.AddIdentityServerAuthentication(options =>
-            //{
-            //    options.Authority = authority;
-            //    options.RequireHttpsMetadata = false;
-            //    options.ApiName = apiResource;
-            //});
+            services.AddSlipwaysData(str);
         }
 
         public void Configure(
@@ -98,8 +65,6 @@ namespace Slipways.API
             }
             app.UseHttpMetrics();
             app.UseRouting();
-            //app.UseAuthentication();
-            //app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();

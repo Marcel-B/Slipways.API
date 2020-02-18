9aa264613d33e785c805bb77223b81f7494abdd0
Marcel Benders <marcel.benders@cleverbridge.com>
2020-02-18 11:43:45 +0100

    (dev) Add UnitTests

--- Slipways.API.Tests/PortControllerTests.cs
+++ Slipways.API.Tests/PortControllerTests.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,6 +21,7 @@ namespace Slipways.API.Tests
         public IList<LogEntry> Logs { get; set; }
         public IList<Port> Ports { get; set; }
         public IList<Slipway> Slipways { get; set; }
+        public Guid PortId { get; set; } = Guid.Parse("FFFAE9C3-C680-4FBD-AB64-D5DE98C78E25");
 
         [SetUp]
         public void SetUp()
@@ -36,12 +36,12 @@ namespace Slipways.API.Tests
             => new PortDto
             {
                 City = "Köln",
-                Id = Guid.NewGuid(),
+                Id = PortId,
                 Name = name,
                 Slipways = new[]
                 {
-                    new SlipwayDto{Id = Guid.NewGuid(), Name = "Test_1"},
-                    new SlipwayDto{Id = Guid.NewGuid(), Name = "Test_2"},
+                    new SlipwayDto{ Id = Guid.NewGuid(), Name = "Test_1", PortFk = PortId },
+                    new SlipwayDto{ Id = Guid.NewGuid(), Name = "Test_2", PortFk = PortId },
                 }
             };
 
@@ -68,22 +68,35 @@ namespace Slipways.API.Tests
                 mock.Setup(_ => _.Port.InsertAsync(It.IsAny<Port>(), It.IsAny<CancellationToken>(), It.IsAny<bool>()))
                     .Callback((Port port, CancellationToken token, bool saveChanges) =>
                     {
-                        port.Id = Guid.NewGuid();
+                        port.Id = PortId;
                         port.Created = DateTime.Now;
                         Ports.Add(port);
                     })
-                    .Returns(Task.FromResult(new Port { Id = Guid.NewGuid(), Name = "Test" }));
+                    .Returns(Task.FromResult(new Port { Id = PortId, Name = "Test" }));
 
             }
             mock.Setup(_ => _.SaveChanges());
+
             mock.Setup(_ => _.Slipway.AddPortToSlipwayAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
-                .Returns(Task.FromResult(new Slipway { Name = "Kirk", Id = Guid.NewGuid() }));
+                .Callback((Guid slipwayId, Guid portId) =>
+                {
+                    Slipways.Add(new Slipway
+                    {
+                        Id = slipwayId,
+                        PortFk = portId,
+                        Name = "Testsliprampe"
+                    });
+                })
+                .Returns(Task.FromResult(new Slipway { Name = "Kirk", Id = Guid.NewGuid(), PortFk = PortId }));
+
             return mock.Object;
         }
+
         private PortController GetSut(
+            Mock<IRepositoryWrapper> mock = null,
             bool errorInsertingValue = false,
             bool throwsException = false)
-            => new PortController(GetRepositoryWrapper(errorInsertingValue, throwsException), GetLogger());
+            => new PortController(mock?.Object ?? GetRepositoryWrapper(errorInsertingValue, throwsException), GetLogger());
 
         [Test]
         public async Task PortController_PostAsync_ReturnsBadRequetsWhenDtoIsNull()
@@ -300,6 +313,52 @@ namespace Slipways.API.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public async Task PortController_PostAsync_AddPortsToSlipwaysIsCalled()
+        {
+            // Arrange
+            var sut = GetSut();
+
+            // Act
+            _ = await sut.PostAsync(GetDto(), CancellationToken.None);
+
+            // Assert
+            Assert.That(Slipways.Count == 2);
+        }
+
+        [Test]
+        public async Task PortController_PostAsync_AddTwoLogEntriesForPortSlipways()
+        {
+            // Arrange
+            var sut = GetSut();
+
+            // Act
+            _ = await sut.PostAsync(GetDto(), CancellationToken.None);
+
+            // Assert
+            Assert.That(Logs.Count == 2);
+        }
+
+        [Test]
+        public async Task PortController_PostAsync_VerifyThatSaveChangesWasCalled()
+        {
+            // Arrange
+            var mock = new Mock<IRepositoryWrapper>();
+            mock.Setup(_ => _.Port.InsertAsync(It.IsAny<Port>(), It.IsAny<CancellationToken>(), It.IsAny<bool>()))
+                .Returns(Task.FromResult(new Port { Id = PortId }));
+
+            mock.Setup(_ => _.Slipway.AddPortToSlipwayAsync(It.IsAny<Guid>(), It.IsAny<Guid>()))
+                .Returns(Task.FromResult(new Slipway { Id = Guid.NewGuid(), Name = "n" }));
+
+            var sut = GetSut(mock);
+
+            // Act
+            _ = await sut.PostAsync(GetDto(), CancellationToken.None);
+
+            // Assert
+            mock.Verify(_ => _.SaveChanges());
         }
     }
 }
